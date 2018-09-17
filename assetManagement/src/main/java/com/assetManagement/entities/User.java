ae227127c1e6954a14ac3bd94dd29f165a3fac40
jesseh <jesseh@jesseh-lt01.Saratoga.local>
2018-09-17 13:19:27 +0200

    Fix

---
+++ assetManagement/src/main/java/com/assetManagement/entities/User.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.assetManagement.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+@Entity
+@Table(name = "User")
+public class User implements Serializable{
+String name,email,password;
+@Id
+    Long userID;
+    private Long id;
+    
+public User(){}
+
+public User(String name,String email,String password,Long userID){
+this.name = name;
+this.email = email;
+this.password = password;
+this.userID  = userID;
+}
+public String getName(){
+return name;
+
+}
+public String getEmail(){
+return email;
+}
+public String getPassword(){
+return password;
+}
+public Long userID(){
+return userID;
+}
+}
