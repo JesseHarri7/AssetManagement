f1a26b99b0fa011938c368f0555847b8028da929
jesseh <jesseh@jesseh-lt01.Saratoga.local>
2018-09-19 11:53:48 +0200

    Update for tests

--- assetManagement/src/test/java/com/assetManagement/entities/AssetAssignedTest.java
+++ assetManagement/src/test/java/com/assetManagement/entities/AssetAssignedTest.java
@@ -2,23 +2,29 @@ package com.assetManagement.entities;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+
 import org.junit.Test;
 
 public class AssetAssignedTest 
 {
+	LocalDate date = LocalDate.of(2018, 8, 19);
 	
 	Asset asset = new Asset("HP Pavlion", "Test", "HP", "2018/09/18", "Good Condition");
-	Employee emp = new Employee();
+	Employee emp = new Employee("Bob", "Marley", "Bob.Marley@gmail.com", date, 1452L);
 	AssetAssigned AA = new AssetAssigned();
 
 	@Test
 	public void testCreateAssetAssigned() 
 	{
 		AA.setAssets(asset);
+		AA.setEmployees(emp);
 		AA.setMoveDate("2018/08/18");
 		
+		assertSame(emp, AA.getEmployees());
 		assertSame(asset, AA.getAssets());
 		assertEquals("2018/08/18", AA.getMoveDate());
+		
 		
 	}
 
