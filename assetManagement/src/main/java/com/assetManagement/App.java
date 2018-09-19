c7003bf1e694634e6ad6a407a91be4d5e8c416af
jesseh <jesseh@jesseh-lt01.Saratoga.local>
2018-09-19 12:53:24 +0200

    App Update

--- assetManagement/src/main/java/com/assetManagement/App.java
+++ assetManagement/src/main/java/com/assetManagement/App.java
@@ -1,15 +1,31 @@
 package com.assetManagement;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import util.HibernateUtil;
 import com.assetManagement.entities.*;
-/**
- * Hello world!
- *
- */
+
+@SpringBootApplication
 public class App 
 {
    public static void main(String args[])
     {
-        System.out.print("test");
+	   SpringApplication.run(App.class, args); 
+	   System.out.print("test");
+	  
+	   /*
+	   Configuration config = new Configuration().configure().addAnnotatedClass(Asset.class);
+	   SessionFactory sf = config.buildSessionFactory();
+	   Session ses = sf.openSession();
+	   */
+	   
+	   Session session = HibernateUtil.getSessionFactory().openSession();
+	   
+       Employee employee = new Employee();
 
     }
 }
