52c9a30776cf5a8d0ff5beac7650256d6fca0ba2
Lutholwethum <Lutholwethum@lutholwethu-lt01.Saratoga.local>
2018-09-20 12:36:59 +0200

    app

--- assetManagement/src/main/java/com/assetManagement/App.java
+++ assetManagement/src/main/java/com/assetManagement/App.java
@@ -1,15 +1,11 @@
 package com.assetManagement;
 
 import com.assetManagement.entities.*;
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
    public static void main(String args[])
     {
         System.out.print("test");
-
     }
 }
