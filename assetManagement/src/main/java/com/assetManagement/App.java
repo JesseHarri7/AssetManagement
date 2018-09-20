3742f4043bec35aab20fd7d9d8e9f4b439fd9c72
jesseh <jesseh@jesseh-lt01.Saratoga.local>
2018-09-20 12:48:13 +0200

    Update

--- assetManagement/src/main/java/com/assetManagement/App.java
+++ assetManagement/src/main/java/com/assetManagement/App.java
@@ -1,8 +1,6 @@
 package com.assetManagement;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.assetManagement.entities.*;
