375e82a6f94eea900012ca4211e7e2e3b3eb13ce
jesseh <jesse.harri@gmail.com>
2018-11-26 14:24:33 +0200

    Update

--- assetManagement/src/main/resources/static/js/assetAssigned.js
+++ assetManagement/src/main/resources/static/js/assetAssigned.js
@@ -1,5 +1,5 @@
-$(document).ready(function()
-{
+/*$(document).ready(function()
+{*/
 	var dataSet = [];
 	//var dataSetA = [];
 	//var dataSetEmp = [];
@@ -7,7 +7,10 @@ $(document).ready(function()
 	var emp;
 	
 	//Global div declaration for alerts
-	var div = document.getElementById('boot-alert');
+	//var div = document.getElementById('boot-alert');
+	
+	//Show cancel button if there is data in local storage
+	showBtn();
 
 	//All data fields on start up
 	findAll();
@@ -29,19 +32,26 @@ $(document).ready(function()
 	//Get Asset info
 	$('#asset-btn').click(function(event)
 	{
+		$('.notifyjs-corner').remove();
+		
 		var table = $('#AA-table').DataTable();
-		var findAssetId = table.row('.selected').data().assets.assetCode;
-		var assetInfo = findAsset(findAssetId);
-		if(assetInfo)
+		var findAssetId = table.row('.selected').data();
+		if(findAssetId)
 		{
+			var assetInfo = findAsset(findAssetId.assets.assetCode);
+			
 			$('#assetModal').modal('show');
 			displayAsset(assetInfo);
 		}
 		else
 		{
-			div.innerHTML = displayAlert("<strong>Warning!</strong> Please select an asset Code.", "alert-warning");
+			$.notify("Heads up! Please select an asset Code.", "warn");
+			
+			//displayAlert("Please select an asset Code.", "warning", "Heads up!");
+			
+			/*div.innerHTML = displayAlert("<strong>Warning!</strong> Please select an asset Code.", "alert-warning");
 			$('#boot-alert').show();
-			slide();
+			slide();*/
 			
 			//alert("Please select an asset Code")
 		}
@@ -50,19 +60,26 @@ $(document).ready(function()
 	//Get Emp info
 	$('#emp-btn').click(function(event)
 	{
+		$('.notifyjs-corner').remove();
+		
 		var table = $('#AA-table').DataTable();
-		var findEmpId = table.row('.selected').data().employees.employeeID;
-		var empInfo = findEmp(findEmpId);
-		if(empInfo)
+		var findEmpId = table.row('.selected').data();
+		if(findEmpId)
 		{
+			var empInfo = findEmp(findEmpId.employees.employeeID);
+			
 			$('#empModal').modal('show');
 			displayEmp(empInfo);			
 		}
 		else
 		{
-			div.innerHTML = displayAlert("<strong>Warning!</strong> Please select an Employee ID.", "alert-warning");
+			$.notify("Heads up! Please select an Employee ID..", "warn");
+			
+			//displayAlert("Please select an Employee ID.", "warning", "Heads up!");
+			
+			/*div.innerHTML = displayAlert("<strong>Warning!</strong> Please select an Employee ID.", "alert-warning");
 			$('#boot-alert').show();
-			slide();
+			slide();*/
 			
 			//alert("Please select an Employee ID")
 		}
@@ -71,29 +88,93 @@ $(document).ready(function()
 	//Delete
 	$('#delete-btn').click(function(event)
 	{
+		$('.notifyjs-corner').remove();
+		
 		var table = $('#AA-table').DataTable();
 
 		var rowToDelete = table.row( '.selected' ).data();
 		
 		if (rowToDelete)
 		{
-			div.innerHTML = displayAlert("Are you sure you want to unassign? <a id='delYes' data-id='Yes' class='alert-link' href='#'>Yes</a>  <a id='delNo' data-id='No' href='#' class='alert-link' >No</a>.", "alert-info");
-			$('#boot-alert').show();
+			createNotify();
+			
+			$.notify({
+				  title: 'Are you sure you want to unassign?',
+				  button: 'Confirm'
+				}, {
+				  style: 'foo',
+				  className: 'info',
+				  autoHide: false,
+				  clickToHide: false
+				});
+			
+			//displayAlert("Are you sure you want to unassign? <p><a href='#' class='alert-link' onclick='delYes();'>Yes</a> <a href='#' class='alert-link' onclick='delNo();' >No</a></p>", "info", "Heads up!");
+			
+			/*div.innerHTML = displayAlert("Are you sure you want to unassign? <a id='delYes' data-id='Yes' class='alert-link' href='#'>Yes</a>  <a id='delNo' data-id='No' href='#' class='alert-link' >No</a>.", "alert-info");
+			$('#boot-alert').show();*/
 		}
 		else
 		{
-			div.innerHTML = displayAlert("<strong>Warning!</strong> Please select a item to unassign.", "alert-warning");
+			$.notify("Heads up! Please select an item to unassign.", "warn");
+			
+			//displayAlert("Please select a item to unassign.", "warning", "Heads up!");
+			
+			/*div.innerHTML = displayAlert("<strong>Warning!</strong> Please select a item to unassign.", "alert-warning");
 			$('#boot-alert').show();
-			slide();
+			slide();*/
 			
 			//alert("Please select a item to remove");
-		}	
+		}
+		
+	});
+	
+	//Notify class
+	function createNotify()
+	{
+		//add a new style 'foo'
+		$.notify.addStyle('foo', {
+		  html: 
+		    "<div>" +
+		      "<div class='clearfix'>" +
+		        "<div class='title' data-notify-html='title'/>" +
+		        "<div class='buttons'>" +
+		          "<button class='btn btn-secondary no'>Cancel</button>" +
+		          "<button class='btn btn-secondary yes' data-notify-text='button'></button>" +
+		        "</div>" +
+		      "</div>" +
+		    "</div>"
+		});
+	}
+	
+	//listen for click events from this style
+	//If no
+	$(document).on('click', '.notifyjs-foo-base .no', function() 
+	{
+		//programmatically trigger propogating hide event
+		$(this).trigger('notify-hide');
+		
+	});
+
+	//if Yes
+	$(document).on('click', '.notifyjs-foo-base .yes', function() 
+	{
+		/*var table = $('#asset-table').DataTable();
+		
+		var test = table.row('.selected').id();
+		table.rows(test).remove().draw( false );*/
+		
+		//Function
+		remove();
+		//hide notification
+		$(this).trigger('notify-hide');
 		
 	});
 	
 	//Reassign button
 	$('#reassign-btn').click(function(event)
 	{
+		$('.notifyjs-corner').remove();
+		
 		var table = $('#AA-table').DataTable();
 
 		var data = table.row( '.selected' ).data();
@@ -107,15 +188,19 @@ $(document).ready(function()
 		}
 		else
 		{
-			div.innerHTML = displayAlert("<strong>Warning!</strong> Please select a item to reassign.", "alert-warning");
+			$.notify("Heads up! Please select a item to reassign.", "warn");
+			
+			//displayAlert("Please select a item to reassign.", "warning", "Heads up!");
+			
+			/*div.innerHTML = displayAlert("<strong>Warning!</strong> Please select a item to reassign.", "alert-warning");
 			$('#boot-alert').show();
-			slide();
+			slide();*/
 			
 			//alert("Please select a item to remove");
 		}	
 	});
 	
-	$('#boot-alert').on('click','a', function() 
+	/*$('#boot-alert').on('click','a', function() 
 	{
 		var $el = $(this);
 		
@@ -127,10 +212,19 @@ $(document).ready(function()
 		{
 			$('#boot-alert').hide();
 		}		
-	});
+	});*/
+	
+	$('#cancel-btn').click(function(event)
+	{
+		$('.notifyjs-corner').remove();
+		
+		resetLocal();
+	})
 	
 	$('#assetT-btn').click( function()
 	{
+		$('.notifyjs-corner').remove();
+		
 		var table = $('#AA-table').DataTable();
 		var findAssetId = table.cell('.selected').data();
 		var assetInfo = findAsset(findAssetId);
@@ -190,16 +284,24 @@ $(document).ready(function()
 			table.row('.selected').remove().draw( false );
 			function success()
 			{
-				div.innerHTML = displayAlert("<strong>Success!</strong> Asset " + rowToDelete.assets.assetCode + " and employee " + rowToDelete.employees.employeeID + " is now unassigned.", "alert-success");
+				$.notify("Success! Asset " + rowToDelete.assets.assetCode + " and employee " + rowToDelete.employees.employeeID + " is now unassigned.", "success");
+				
+				//displayAlert("Asset " + rowToDelete.assets.assetCode + " and employee " + rowToDelete.employees.employeeID + " is now unassigned.", "success", "Success!");
+				
+				/*div.innerHTML = displayAlert("<strong>Success!</strong> Asset " + rowToDelete.assets.assetCode + " and employee " + rowToDelete.employees.employeeID + " is now unassigned.", "alert-success");
 				$('#boot-alert').show();
 				//slide();
-			}
+*/			}
 		}
 		else
 		{
-			div.innerHTML = displayAlert("<strong>Warning!</strong> Please select a item to unassign.", "alert-warning");
+			$.notify("Heads up! Please select a item to unassign.", "warn");
+			
+			//displayAlert("Please select a item to unassign.", "warning", "Heads up!");
+			
+			/*div.innerHTML = displayAlert("<strong>Warning!</strong> Please select a item to unassign.", "alert-warning");
 			$('#boot-alert').show();
-			slide();
+			slide();*/
 			
 			//alert("Please select a item to remove");
 		}	
@@ -268,20 +370,6 @@ $(document).ready(function()
 		
 	}
 	
-	function slide()
-	{
-		$('#boot-alert').fadeTo(5000, 900).slideUp(900, function(){
-			$('#boot-alert').slideUp(900);
-		});
-	}
-	
-	function displayAlert(msg, type)
-	{
-		var alert = "<div class='alert " + type + " alert-dismissible fade in'> <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a> "
-						+ msg + "</div>";
-		return alert;
-	}
-	
 	function selectAA()
 	{
 		var table = $('#AA-table').DataTable();
@@ -297,11 +385,66 @@ $(document).ready(function()
 		}
 		else
 		{
-			div.innerHTML = displayAlert("<strong>Warning!</strong> Please select a item to reassign.", "alert-warning");
+			$.notify("Heads up! Please select a item to reassign.", "warn");
+			
+			//displayAlert("Please select a item to reassign.", "warning", "Heads up!");
+			
+			/*div.innerHTML = displayAlert("<strong>Warning!</strong> Please select a item to reassign.", "alert-warning");
 			$('#boot-alert').show();
-			slide();
+			slide();*/
 			//alert("Please select a row");
 		}
 	}
 	
-});
+	function resetLocal()
+	{
+		localStorage.clear();
+		$('#cancel-btn').hide();
+		$.notify("Success! Action cancelled.", "success");
+		
+		//displayAlert("Action cancelled", "success", "Success!");
+	}
+	
+	function showBtn()
+	{
+		if(localStorage.length > 0)
+		{
+			$('#cancel-btn').addClass('visible');
+		}
+	}
+	
+/*	function displayAlert(msg, type, title)
+	{
+		$.notify({
+			title: '<strong>' + title + '</strong>',
+			message: msg
+		},{
+			animate: 
+			{
+				enter: 'animated fadeInRight',
+				exit: 'animated fadeOutRight'
+			},
+			type: type,
+			delay: 10000,
+			timer: 1000,
+			mouse_over: 'pause',
+			z_index: 2000
+			
+		});
+	}*/
+	
+	/*function slide()
+	{
+		$('#boot-alert').fadeTo(5000, 900).slideUp(900, function(){
+			$('#boot-alert').slideUp(900);
+		});
+	}
+	
+	function displayAlert(msg, type)
+	{
+		var alert = "<div class='alert " + type + " alert-dismissible fade in'> <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a> "
+						+ msg + "</div>";
+		return alert;
+	}*/
+	
+//});
